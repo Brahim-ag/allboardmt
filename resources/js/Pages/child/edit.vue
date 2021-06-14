0f3e93032a8b1dd3f9ddb40279bf7990443dca50
Brahim-ag <54471586+Brahim-ag@users.noreply.github.com>
2021-06-14 14:12:28 +0100

    Modification enfant

--- resources/js/Pages/child/edit.vue
+++ resources/js/Pages/child/edit.vue
@@ -2,7 +2,7 @@
     <app-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Modifer un employé
+                Modification enfant
             </h2>
         </template>
        <div>
