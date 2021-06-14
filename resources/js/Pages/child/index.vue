d9f5ecb36d12f65e53a8abf16bc40ef595ecdb38
Brahim-ag <54471586+Brahim-ag@users.noreply.github.com>
2021-06-14 14:08:16 +0100

    Enfants

--- resources/js/Pages/child/index.vue
+++ resources/js/Pages/child/index.vue
@@ -2,16 +2,10 @@
   <app-layout>
     <template #header>
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        childs Scolaire
+        Enfants
       </h2>
     </template>
     <div class="max-w-9xl mx-auto py-10 sm:px-2 lg:px-8">
-      <inertia-link
-        :href="`/child/create/${this.employe_id}`"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded"
-        >Ajouter</inertia-link
-      >
-
       <div
         class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"
       >
@@ -49,6 +43,10 @@
           </tr>
         </table>
       </div>
+      <inertia-link
+        :href="`/child/create/${this.employe_id}`"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded"
+        >Ajouter</inertia-link>
     </div>
   </app-layout>
 </template>
