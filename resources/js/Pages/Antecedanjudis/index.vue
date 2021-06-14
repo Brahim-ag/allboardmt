fa5340ff076dc3eed3ba1f54f056f310a33d1074
Brahim-ag <54471586+Brahim-ag@users.noreply.github.com>
2021-06-14 13:45:43 +0100

    Antécédents Judiciaires

--- resources/js/Pages/Antecedanjudis/index.vue
+++ resources/js/Pages/Antecedanjudis/index.vue
@@ -2,7 +2,7 @@
     <app-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                antecejudis
+                Antécédents Judiciaires
             </h2>
         </template>
         <div class="flex max-w-8xl mx-auto py-10 sm:px-2 lg:px-8">
@@ -113,7 +113,7 @@
                         </tbody>
 
                         <inertia-link
-                            :href="`/antecejudi/create/${this.employe_id}`"
+                            :href="`/antecejudi/create/${this.employe_id}`" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded"
                             >Ajouter</inertia-link
                         >
                     </table>
