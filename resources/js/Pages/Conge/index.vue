ae87ceb878a6b26cfd7d4411daf16912bf15de96
Brahim-ag <54471586+Brahim-ag@users.noreply.github.com>
2021-06-14 17:07:34 +0100

    Congés

--- resources/js/Pages/Conge/index.vue
+++ resources/js/Pages/Conge/index.vue
@@ -1,7 +1,7 @@
 <template>
   <app-layout>
     <template #header>
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">conge</h2>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Congés</h2>
     </template>
     <div class="max-w-7xl mx-auto py-10 sm:px-2 lg:px-8">
       <table class="min-w-full divide-y divide-gray-200">
@@ -27,7 +27,7 @@
           </td>
         </tr>
       </table>
-      <inertia-link :href="`/conge/create/${this.employe_id}`"
+      <inertia-link :href="`/conge/create/${this.employe_id}`" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded"
         >Ajouter</inertia-link
       >
     </div>
