772ff0ff63d6354307da0316b010edabfbe96658
Brahim-ag <54471586+Brahim-ag@users.noreply.github.com>
2021-06-14 16:30:05 +0100

    Hors-Cadre

--- resources/js/Pages/Horscadres/index.vue
+++ resources/js/Pages/Horscadres/index.vue
@@ -2,7 +2,7 @@
     <app-layout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                horscadre
+                Hors-Cadre
             </h2>
         </template>
         <div class="max-w-7xl mx-auto py-10 sm:px-2 lg:px-8"> 
@@ -41,7 +41,7 @@
                     </td>
                 </tr>
             </table>
-            <inertia-link :href="`/horscadre/create/${this.employe_id}`">Ajouter</inertia-link>
+            <inertia-link :href="`/horscadre/create/${this.employe_id}`" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">Ajouter</inertia-link>
         </div>
     </app-layout>
 </template>
