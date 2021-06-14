ded9f8a8b11ab668bbec5568692e6b7c30fff11b
Brahim-ag <54471586+Brahim-ag@users.noreply.github.com>
2021-06-14 17:04:48 +0100

    Nouveau Regime Disciplinaire

--- resources/js/Pages/Regim/create.vue
+++ resources/js/Pages/Regim/create.vue
@@ -2,7 +2,7 @@
   <app-layout>
     <template #header>
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        Regime Disciplinaire
+        Nouveau Regime Disciplinaire
       </h2>
     </template>
     <div class="container mx-auto shadow overflow-hidden sm:rounded-md">
